84d8dacb24f9649e1215ec8595e50316faf606d8
Sarah Uckan <suckan@softheon.com>
2018-07-16 12:12:52 -0400

    Routing Fix

--- projects/workshop/src/lib/multi-stepper/multi-stepper.module.ts
+++ projects/workshop/src/lib/multi-stepper/multi-stepper.module.ts
@@ -4,17 +4,12 @@ import { MultiStepperComponent } from './multi-stepper.component';
 import { RouterModule, Routes } from '@angular/router';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-const routes: Routes = [{ path: 'multistepper', component: MultiStepperComponent}];
-
 /** The NG-Workshop Multi Stepper Module */
 @NgModule({
   imports: [
     CommonModule,
     BrowserAnimationsModule,
-    RouterModule.forRoot(
-      routes,
-      // { enableTracing: true } // <-- debugging purposes only
-    )
+    RouterModule
   ],
   declarations: [MultiStepperComponent],
   exports: [MultiStepperComponent]
